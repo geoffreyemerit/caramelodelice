db8e55bf95423abbbb775892708953c99689c00c
EstelleChardac <estelle.chardac@gmail.com>
2022-06-17 12:43:19 +0200

    footer scss responsive fix

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,29 +3,29 @@ import Logo from './Logo';
 
 const Footer = () => {
   return (
-    <div className="container">
-      <div className="container__info">
-        <div className="container__info__sup">
-          <div className="container__info__sup__address">
-            <span className="container__info__sup__address__street">
+    <div className="footer">
+      <div className="footer__info">
+        <div className="footer__info__sup">
+          <div className="footer__info__sup__address">
+            <span className="footer__info__sup__address__street">
               24, RUE D&apos;ESPAGNE
             </span>
             <span>64200 BIARRITZ</span>
           </div>
-          <div className="container__info__sup__opening">
-            <div className="container__info__sup__opening__openingdays">
+          <div className="footer__info__sup__opening">
+            <div className="footer__info__sup__opening__openingdays">
               <p>MAR-JEU</p>
               <p>VEN-SAM</p>
               <p>DIM</p>
             </div>
-            <div className="container__info__sup__opening__openinghours">
+            <div className="footer__info__sup__opening__openinghours">
               <p>11:00-15:00 / 18:00-23:00 </p>
               <p>11:00-15:00 / 18:00-00:00</p>
               <p>11:00-14:00</p>
             </div>
           </div>
         </div>
-        <div className="container__logo">
+        <div className="footer__logo">
           <a
             href="https://instagram.com/club_sandwich64?igshid=YmMyMTA2M2Y="
             target="_blank"
