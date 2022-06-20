207c466389a39f4b983b02110ce0346f3c99a3c7
EstelleChardac <estelle.chardac@gmail.com>
2022-06-20 16:50:37 +0200

    sound checkbox

--- src/components/global/Sound.tsx
+++ src/components/global/Sound.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sound = () => {
+  const [isActive, setIsActive] = useState(false);
+  const handleChange = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <div>
-      <h2>Sound</h2>
-    </div>
+    <>
+      <div className="sound">
+        <div className="sound__soundcheck">
+          <div
+            className={
+              isActive ? 'sound__soundcheck__box--active' : 'sound__soundcheck__box'
+            }
+            onClick={handleChange}>
+            {/* <embed src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1449724630"></embed> */}
+          </div>
+        </div>
+        <span className="sound__soundcheck__title">SOUND</span>
+      </div>
+    </>
   );
 };
+
+{
+  /* <embed src="https://soundcloud.com/elsa-rampazzo" autostart="true"></embed> */
+}
 
 export default Sound;
