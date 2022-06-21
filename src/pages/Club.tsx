94946c7b2504007df4084700eeeb10e98aac437d
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-06-21 15:48:53 +0200

    okokk

--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ClubSection from '../components/club/ClubSection';
 
 const Club = () => {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
+
 import Footer from '../components/global/Footer';
 import Logo from '../components/global/Logo';
+import Navbar from '../components/global/Navbar';
 import Sound from '../components/global/Sound';
 
 const Home = () => {
   return (
     <div className="home">
-      <Logo myStyle="icon icon--menu" icon="logos-menu-burger" />
-      <h1>WELCOME TO THE CLUB SANDWICH</h1>
+      <Navbar />
+      <Logo myStyle="home__csLogo" icon="logos-club-sandwich-logo" />
+      <h1 className="home__title">
+        WELCOME TO <span className="home__title--club">THE CLUB</span>
+      </h1>
       {/* Exemple pour appeler le logo.svg / 
       myStyle = nom de la classe pour le SCSS
-      icon = nom de l'id dans le fichier SVG (dans assets) */}
+      icon = nom de l'id dans le fichier SVG (dans assets) 
+            <Logo myStyle="icon icon--menu" icon="logos-club-sandwich-logo" />
+*/}
+      <Sound />
 
+      <p className="home__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum distinctio quidem
+        doloribus a quae ipsa, fugit ratione aut error ad illo labore repellat cum laborum
+        possimus voluptatum esse maiores recusandae?
+      </p>
+      <p className="home__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum distinctio quidem
+        doloribus a quae ipsa, fugit ratione aut error ad illo labore repellat cum laborum
+        possimus voluptatum esse maiores recusandae?
+      </p>
       <Footer />
-      <p>blablabla</p>
     </div>
   );
 };
