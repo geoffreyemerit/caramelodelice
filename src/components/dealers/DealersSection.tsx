c1c45bface36cb44365c873329652679dbd06e12
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-06-27 09:28:24 +0200

    initialized dealers page with axios

---
+++ src/components/dealers/DealersSection.tsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import IPage from '../../interfaces/IPage';
+import ISupplier from '../../interfaces/ISupplier';
+
+interface DealersSectionProps {
+  id: number;
+  idSupplier: number;
+}
+
+const DealersSection = ({ id, idSupplier }: DealersSectionProps) => {
+  // JE CRÉE UN USESTATE AFIN DE STOCKER LA DONNÉE DE MA SECTION ISSU DE L'APPEL AXIOS DANS "PAGE"
+  const [page, setPage] = useState<IPage>();
+  const [supplier, setSupplier] = useState<ISupplier>();
+
+  // APPEL API AXIOS POUR 1 SECTION
+  const getPage = async () => {
+    const dealersPage = await axios.get<IPage>(`http://localhost:3000/api/pages/${id}`);
+    setPage(dealersPage.data);
+  };
+
+  // APPEL API AXIOS POUR 1 SUPPLIER RELIÉ À 1 SECTION
+  const getSupplier = async () => {
+    const supplierContent = await axios.get<ISupplier>(
+      `http://localhost:3000/api/suppliers/${idSupplier}`,
+    );
+    setSupplier(supplierContent.data);
+  };
+
+  // AU CHARGEMENT DU COMPOSANT, J'EXÉCUTE LA FONCTION GETPAGE
+  useEffect(() => {
+    getPage();
+    getSupplier();
+  }, []);
+
+  return (
+    <div className="dealersSection">
+      {page && supplier && (
+        <>
+          <div className="dealersSection__text">
+            <h2 className="dealersSection__text__subTitle">{page.subTitle}</h2>
+            <h1 className="dealersSection__text__title">{page.title}</h1>
+          </div>
+          <img className="dealersSection__image1" src={page.image1} alt={page.title} />
+          <div className="dealersSection__supplier">
+            <h3 className="dealersSection__supplier__name">{supplier.name}</h3>
+            <h4 className="dealersSection__supplier__location">{supplier.location}</h4>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DealersSection;
--- src/pages/Dealers.tsx
+++ src/pages/Dealers.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import DealersSection from '../components/dealers/DealersSection';
 
 const Dealers = () => {
   return (
     <div>
-      <h1>DEALERS</h1>
+      <DealersSection id={6} idSupplier={1} />
+      <DealersSection id={7} idSupplier={2} />
+      <DealersSection id={8} idSupplier={3} />
+      <DealersSection id={9} idSupplier={4} />
+      <DealersSection id={10} idSupplier={5} />
     </div>
   );
 };
