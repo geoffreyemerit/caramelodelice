9eef42ed5f26815853cda95680b12c42c3d6301f
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-07-05 09:34:28 +0200

    [BUGFIX] adapting reponsive

--- src/pages/Dealers.tsx
+++ src/pages/Dealers.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import DealersMobileSection from '../components/dealers/DealersMobileSection';
 import DealersSection from '../components/dealers/DealersSection';
 
