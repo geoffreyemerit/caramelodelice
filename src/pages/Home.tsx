4553453f6d0c3100cb74564f68fc5ad133121512
EstelleChardac <estelle.chardac@gmail.com>
2022-07-11 11:53:19 +0200

    ipad + mobile home 70% completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,19 +8,10 @@ const Home = () => {
     <>
       <div className="home">
         <HomeContent id={41} />
-        <Footer className="home__footer" />
+        <Footer className="footer" />
       </div>
     </>
   );
 };
 
 export default Home;
-
-/* <h1>
-        WELCOME TO <span className="home__title--club">THE CLUB</span>
-      </h1>
-      {/* Exemple pour appeler le logo.svg / 
-      myStyle = nom de la classe pour le SCSS
-
-      icon = nom de l'id dans le fichier SVG (dans assets) 
-            <Logo myStyle="icon icon--menu" icon="logos-club-sandwich-logo" />*/
