46dbdb5293549fe30c12b8678deeb9c97871d71b
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-07-20 08:56:46 +0200

    mapping club section init

--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ScrollDown from '../components/global/ScrollDown';
 import ClubSection from '../components/club/ClubSection';
 import Footer from '../components/global/Footer';
 import GoToTop from '../components/global/GoToTop';
+import axios from 'axios';
+import IPage from '../interfaces/IPage';
 
 const Club = () => {
+  // JE CRÉE UN USESTATE AFIN DE STOCKER LA DATA ISSU DE L'APPEL AXIOS DANS CONTENT
+  const [content, setContent] = useState<IPage>();
+
+  // APPEL API AXIOS
+  const getContent = async () => {
+    const url: string = `${import.meta.env.VITE_API_URL}/api/pageTypes/2`;
+    const { data } = await axios.get<IPage>(url);
+    setContent(data);
+  };
+
+  // AU CHARGEMENT DU COMPOSANT, J'EXÉCUTE LA FONCTION GETCONTENT
+  useEffect(() => {
+    getContent();
+  }, []);
+
   return (
     <div className="club">
       <ScrollDown />
-      <ClubSection id={1} />
-      <ClubSection id={11} />
-      <ClubSection id={21} />
-      <ClubSection id={31} />
+      {content &&
+        content.map((section) => <ClubSection id={section.id} key={content.id} />)}
       <Footer className="footer" />
       <GoToTop />
     </div>
