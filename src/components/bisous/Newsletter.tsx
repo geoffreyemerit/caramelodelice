ef321b880f4f333936810d91d985b773bbbb8760
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-07-20 09:14:07 +0200

    mapping club section init

--- src/components/bisous/Newsletter.tsx
+++ src/components/bisous/Newsletter.tsx
@@ -1,10 +1,11 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import IPage from '../../interfaces/IPage';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+
 import INewsletterSubscriber from '../../interfaces/INewsletterSubscriber';
+import IPage from '../../interfaces/IPage';
 
 interface NewsletterProps {
   id: number;
--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+import React from 'react';
 
-import ScrollDown from '../components/global/ScrollDown';
-import ClubSection from '../components/club/ClubSection';
+// import ClubSection from '../components/club/ClubSection';
 import Footer from '../components/global/Footer';
 import GoToTop from '../components/global/GoToTop';
-import axios from 'axios';
-import IPage from '../interfaces/IPage';
+import ScrollDown from '../components/global/ScrollDown';
+// import IPage from '../interfaces/IPage';
 
 const Club = () => {
   // JE CRÉE UN USESTATE AFIN DE STOCKER LA DATA ISSU DE L'APPEL AXIOS DANS CONTENT
-  const [content, setContent] = useState<IPage>();
+  // const [content, setContent] = useState<IPage>();
 
-  // APPEL API AXIOS
-  const getContent = async () => {
-    const url: string = `${import.meta.env.VITE_API_URL}/api/pageTypes/2`;
-    const { data } = await axios.get<IPage>(url);
-    setContent(data);
-  };
+  // // APPEL API AXIOS
+  // const getContent = async () => {
+  //   const url: string = `${import.meta.env.VITE_API_URL}/api/pageTypes/2`;
+  //   const { data } = await axios.get<IPage>(url);
+  //   setContent(data);
+  // };
 
-  // AU CHARGEMENT DU COMPOSANT, J'EXÉCUTE LA FONCTION GETCONTENT
-  useEffect(() => {
-    getContent();
-  }, []);
+  // // AU CHARGEMENT DU COMPOSANT, J'EXÉCUTE LA FONCTION GETCONTENT
+  // useEffect(() => {
+  //   getContent();
+  // }, []);
 
   return (
     <div className="club">
       <ScrollDown />
-      {content &&
-        content.map((section) => <ClubSection id={section.id} key={content.id} />)}
+      {/* {content &&
+        content.map((section) => <ClubSection id={section.id} key={content.id} />)} */}
       <Footer className="footer" />
       <GoToTop />
     </div>
